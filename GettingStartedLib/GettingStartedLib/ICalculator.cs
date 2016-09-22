f27f44e23b54a1b10e591245ff01a7f9b97527c9
Hyman <1604723751@qq.com>
2016-09-22 17:44:20 +0800

    Add the first wcf demo to the solution

---
+++ GettingStartedLib/GettingStartedLib/ICalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GettingStartedLib
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+
+    // 使用下面示例中说明的数据约定将复合类型添加到服务操作。
+    // 可以将 XSD 文件添加到项目中。在生成项目后，可以通过命名空间“GettingStartedLib.ContractType”直接使用其中定义的数据类型。
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
